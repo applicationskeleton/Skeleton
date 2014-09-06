91266e4c195c983ce60b904e9f6528a11690b002
Zhao Zhang <zhangzhao0718@gmail.com>
2014-09-06 14:28:56 -0700

    adding a local tc.data generator

---
+++ util/gen-tc-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path=`which task`
+
+if [ $path == "" ]; then
+    echo "The path of task is not exported"
+    echo "Please run:"
+    echo "export PATH=$PATH:/path-to-task"
+    exit 1
+fi
+
+echo -e "localhost\ttask\t${path}\tINSTALLED\tINTEL32::LINUX\tnull"
