569fdd7fcf9c52f62b69da636c1a220777d02caa
Zhao Zhang <zhangzhao0718@gmail.com>
2014-09-09 11:06:42 -0700

    fix string equality bug

--- util/gen-tc-data.sh
+++ util/gen-tc-data.sh
@@ -2,10 +2,10 @@
 
 path=`which task`
 
-if [ $path == "" ]; then
+if [[ $path == "" ]]; then
     echo "The path of task is not exported"
     echo "Please run:"
-    echo "export PATH=$PATH:/path-to-task"
+    echo 'export PATH=$PATH:/path-to-task'
     exit 1
 fi
 
