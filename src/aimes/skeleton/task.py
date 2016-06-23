38c3aac8b707007dd7945f21693c28b3cc0d0900
Andre Merzky <andre@merzky.net>
2016-06-23 14:08:57 +0200

    link task.py to synapse
    $ time ./src/aimes/skeleton/task.py serial 1 10 65536 65536 2 1 0 Stage_1_Input/Stage_1_Input_0_1 Stage_1_Input/Stage_1_Input_1_1 Stage_1_Output/Stage_1_Output_0_1 1048576
    serial: 13
    process 1/1 reading input files
    emulating 2 samples
    emulate  {'rsize': 1048576, 'src': 'Stage_1_Input/Stage_1_Input_0_1', 'buf': 65536, 'tgt': '/tmp/synapse_storage.%(pid)s.out', 'wsize': 0}
    emulate  {'rsize': 1048576, 'src': 'Stage_1_Input/Stage_1_Input_1_1', 'buf': 65536, 'tgt': '/tmp/synapse_storage.%(pid)s.out', 'wsize': 0}
    process 1/1 is sleeping
    emulating 1 samples
    emulate  {'efficiency': 1, 'flops': 0, 'time': 10}
    process 1/1 writing output files
    emulating 1 samples
    emulate  {'rsize': 0, 'src': '/tmp/synapse_storage.in', 'buf': 65536, 'tgt': 'Stage_1_Output/Stage_1_Output_0_1', 'wsize': 1048576}
    r:0m11.304s  u:0m10.656s  s:0m0.248s
    
    $ time ./src/aimes/skeleton/task serial 1 10 65536 65536 2 1 0 Stage_1_Input/Stage_1_Input_0_1 Stage_1_Input/Stage_1_Input_1_1 Stage_1_Output/Stage_1_Output_0_1 1048576
    serial: 13
    reading input file: Stage_1_Input/Stage_1_Input_0_1 size: 1048576 num_reads: 16
    read : 1048576 bytes to Stage_1_Input/Stage_1_Input_0_1 with bufsize: 65536
    reading input file: Stage_1_Input/Stage_1_Input_1_1 size: 1048576 num_reads: 16
    read : 1048576 bytes to Stage_1_Input/Stage_1_Input_1_1 with bufsize: 65536
    sleep interval: 10.000000
    write: 1048576 bytes to Stage_1_Output/Stage_1_Output_0_1 with bufsize: 65536
    r:0m10.623s  u:0m0.032s  s:0m0.484s

--- src/aimes/skeleton/task.py
+++ src/aimes/skeleton/task.py
@@ -4,13 +4,14 @@ import os
 import sys
 import stat
 import time
+import pprint
 
 import radical.utils   as ru
 import radical.synapse as rs
 
 # ------------------------------------------------------------------------------
 #
-MPI = False
+mpi = False
 
 if mpi:
     from mpi4py import MPI as mpi
@@ -35,133 +36,177 @@ def rand_str(length):
 
 # ------------------------------------------------------------------------------
 #
-def prep_output(fname):
-
-    dname = os.path.dirname(fname)
-
-    try:
-        os.path.mkdirs(dname)
-    except os.error:
-        # dir exists
-        pass
-
-    os.chmod (dname, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-
-
-# ------------------------------------------------------------------------------
-#
 def readfiles(input_files, bufsize):
 
-    for input_file in input_files
+    samples = list()
+    n       = 0
 
-        count      = 0;
-        total_size = 0;
+    for input_file in input_files:
 
-        fd    = os.open(input_file, 'r')
+        n    += 1
         fsize = os.stat(input_file).st_size
+        samples.append(['sto', n,  {'src'        : input_file, 
+                                    'rsize'      : fsize, 
+                                    'tgt'        : None, 
+                                    'wsize'      : 0, 
+                                    'buf'        : bufsize}])
 
-        print "reading input file: %s fsize: %ld num_reads: %d" % \
-                (input_file, fsize, fsize/bufsize)
-
-        while total_size < fsize:
-
-            buffer = read(fd, min(bufsize, fsize - total_size))
-
-            if len(buffer) < 0:
-                raise RuntimeError("cannot read from %s" % input_file)
-
-            # print "read: %s" % buffer
-            total_size += len(buffer)
-            count      += 1
-
-            if not count % 1000:
-                print "reading operations: %d total_size: %d st_size: %d" % \
-                       (count, total_size, fsize)
-
-        print "read : %d bytes to %s with bufsize: %d" % \
-                (total_size, input_file, bufsize)
-
-        os.close(fd)
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
 
 # ------------------------------------------------------------------------------
 #
 def writefiles(output_files, output_sizes, bufsize):
 
-    buffer = ""
+    samples = list()
+    n       = 0
 
     for output_file, output_size in zip(output_files, output_sizes):
 
-        fd = None
-        size;
-        count = 0;
-        total_size = 0;
+        n += 1
+        samples.append(['sto', n,  {'src'        : None, 
+                                    'rsize'      : 0, 
+                                    'tgt'        : output_file, 
+                                    'wsize'      : output_size, 
+                                    'buf'        : bufsize}])
 
-        prep_output(output_files)
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
-        fd = os.open(output_file, 'w+')
 
-        while total_size < output_size:
-
-            buffer = rand_str(bufsize)
-            size = os.write(fd, buffer, min(bufsize, output_sizes[i] - total_size))
-
-            if size < 0:
-                raise RuntimeError("cannot write to %s" % output_files)
-
-            os.fsync(fd)
-            total_size += size;
-            count      += 1
-
-            if not count % 1000:
-                print "write operations: %d total_size: %d output_size: %d" % \ 
-                       (count, total_size, output_size)
-
-        print "write: %d bytes to %s with bufsize: %d" % \
-                (total_size, output_files[i], bufsize)
-
-        os.close(fd)
 
 
 # ------------------------------------------------------------------------------
 #
 def compute(task_length):
 
-    print "sleep interval: %f" % task_length
-    time.sleep(task_length)
+    samples = list()
+    samples.append(['cpu', 0, {'time'       : task_length, 
+                               'flops'      : 0, 
+                               'efficiency' : 1}])
 
-
-# ------------------------------------------------------------------------------
-#
-# Run floating point calculations as a token of work
-# compute_flop with run task_length number of floating point
-# operations
-# 
-def compute_flop(task_length):
-    
-    pass
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
 
 # ------------------------------------------------------------------------------
 #
 def read_compute(input_files, bufsize, task_length):
 
-    pass
+    total_size = 0
+    for input_file in input_files:
+        total_size += os.stat(input_file).st_size
+
+    nreads    = int(total_size / bufsize)
+    cpu_chunk = int(task_length / nreads)
+
+    samples = list()
+    n       = 0
+
+    for input_file in input_files:
+
+        fsize     = os.stat(input_file).st_size / bufsize
+        n_samples = fsize / bufsize
+
+        for s in range(n_samples):
+
+            # construct artificial time)
+            n += 1
+            samples.append(['cpu', t, {'time'       : cpu_chunk, 
+                                       'flops'      : 0, 
+                                       'efficiency' : 1}])
+
+            samples.append(['sto', t,  {'src'        : input_file, 
+                                        'rsize'      : bufsize, 
+                                        'tgt'        : None, 
+                                        'wsize'      : 0, 
+                                        'buf'        : bufsize}])
+
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
 
 # ------------------------------------------------------------------------------
 #
-def compute_write (task_length, output_files, output_sizes, bufsize):
+def compute_write(task_length, output_files, output_sizes, bufsize):
 
-    pass
+    total_size = 0
+    for output_size in output_sizes:
+        total_size += output_size
+
+    nreads    = int(total_size / bufsize)
+    cpu_chunk = int(task_length / nreads)
+
+    samples = list()
+    n       = 0
+
+    for output_file, output_size in zip(output_files, output_sizes):
+
+        fsize     = output_size
+        n_samples = fsize / bufsize
+
+        for s in range(n_samples):
+
+            n += 1
+            samples.append(['cpu', n, {'time'       : cpu_chunk, 
+                                       'flops'      : 0, 
+                                       'efficiency' : 1}])
+
+            samples.append(['sto', n,  {'src'        : None, 
+                                        'rsize'      : 0, 
+                                        'tgt'        : output_size, 
+                                        'wsize'      : bufsize, 
+                                        'buf'        : bufsize}])
+
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
 
 # ------------------------------------------------------------------------------
 #
-def read_compute_write(input_files, r_bufsize, task_length, 
-        output_files, output_sizes, w_bufsize):
+def read_compute_write(input_files, r_bufsize, 
+                       task_length, 
+                       output_files, output_sizes, w_bufsize):
 
-    pass
+    # we chunk the compute according to output chunking
+    total_size = 0
+    for output_size in output_sizes:
+        total_size += output_size
+
+    nreads    = int(total_size / bufsize)
+    cpu_chunk = int(task_length / nreads)
+
+    samples = list()
+    n       = 0
+
+    for input_file, output_file, output_size in zip(input_files, output_files, output_sizes):
+
+        r_size    = os.stat(input_file).st_size
+        w_size    = output_size
+        n_samples = fsize / bufsize
+
+        for s in range(n_samples):
+
+            n += 2
+            samples.append(['sto', t,  {'src'          : input_file, 
+                                        'rsize'        : int(r_size/n_samples), 
+                                        'tgt'          : None, 
+                                        'wsize'        : 0, 
+                                        'buf'          : r_bufsize}])
+
+            samples.append(['cpu', t+1, {'time'        : cpu_chunk, 
+                                         'flops'       : 0, 
+                                         'efficiency'  : 1}])
+
+            samples.append(['sto', t+1,  {'src'        : None, 
+                                          'rsize'      : 0, 
+                                          'tgt'        : output_file, 
+                                          'wsize'      : int(w_size/n_samples), 
+                                          'buf'        : w_bufsize}])
+
+    info, ret, out = rs.emulate(samples=samples)
+  # pprint.pprint(info)
 
 
 # ------------------------------------------------------------------------------
@@ -187,13 +232,15 @@ def main(argv):
     else:
         print "serial: %d" % argc
         master = True
+        rank   = 1
+        size   = 1
 
 
-    if ( argc < 9 )
+    if argc < 9:
         raise ValueError("insufficient arguments")
 
     # input parameter processing
-    type_s         = str(argv[1]
+    type_s         = str(argv[1])
     num_proc       = int(argv[2])
     task_length    = float(argv[3])
     read_buf       = int(argv[4])
@@ -202,15 +249,15 @@ def main(argv):
     num_output     = int(argv[7])
     interleave_opt = int(argv[8])
 
-    if (num_proc       <= 0) : raise ValueError("invalid value for num_proc")
-    if (task_length    <  0) : raise ValueError("invalid value for task_length")
-    if (read_buf       <= 0) : raise ValueError("invalid value for read_buf")
-    if (write_buf      <= 0) : raise ValueError("invalid value for write_buf")
-    if (num_input      <  0) : raise ValueError("invalid value for num_input")
-    if (num_output     <  0) : raise ValueError("invalid value for num_output")
-    if (interleave_opt <  0) : raise ValueError("invalid value for interleave_opt")
+    if num_proc       <= 0 : raise ValueError("invalid value for num_proc")
+    if task_length    <  0 : raise ValueError("invalid value for task_length")
+    if read_buf       <= 0 : raise ValueError("invalid value for read_buf")
+    if write_buf      <= 0 : raise ValueError("invalid value for write_buf")
+    if num_input      <  0 : raise ValueError("invalid value for num_input")
+    if num_output     <  0 : raise ValueError("invalid value for num_output")
+    if interleave_opt <  0 : raise ValueError("invalid value for interleave_opt")
 
-    if ( argc < (9 + num_input + (2*num_output)) ):
+    if argc < (9 + num_input + (2*num_output)):
         raise ValueError("insufficient arguments")
 
   # print "task type     : %s" & type
@@ -236,7 +283,6 @@ def main(argv):
         output_sizes.append(int(argv[10+num_input+i*2]))
       # print "%dth output file name: %s, size: %d" % \
       #       (i, output_names[i], # output_sizes[i])
-    }
 
 
     if not interleave_opt:
@@ -244,8 +290,8 @@ def main(argv):
         if master:
             print "process %d/%d reading input files" % (rank, size)
 
-         # read input files*/
-         readfiles(input_names, read_buf)
+        # read input files*/
+        readfiles(input_names, read_buf)
 
         if mpi:
             comm.Barrier()
@@ -256,10 +302,11 @@ def main(argv):
         # compute
         compute(task_length)
 
-        comm.Barrier()
+        if mpi:
+            comm.Barrier()
 
         if master:
-            print "process %d/%d writing output files" % (rank, scale)
+            print "process %d/%d writing output files" % (rank, size)
 
         # write output files
         writefiles(output_names, output_sizes, write_buf)
@@ -292,7 +339,6 @@ def main(argv):
         print "interleave input, compute, and write"
         read_compute_write(input_names, read_buf, num_input, task_length, 
                            output_names, output_sizes, write_buf, num_output);
-    }
 
     return 0
 
@@ -301,7 +347,7 @@ def main(argv):
 #
 if __name__ == '__main__':
 
-    sys.exit main(sys.argv)
+    sys.exit(main(sys.argv))
 
 
 # ------------------------------------------------------------------------------
